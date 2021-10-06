ec93b3a5d8e4e9a904430b4688dd4804d8e2398e
zarosath <zarosath@gmail.com>
2021-10-06 13:26:29 -0600

    files

---
+++ .bmx/Terrain.bmx.gui.release.win32.x64.h
@@ -0,0 +1,9 @@
+#ifndef TERRAIN_TERRAIN_BMX_GUI_RELEASE_WIN32_X64_H
+#define TERRAIN_TERRAIN_BMX_GUI_RELEASE_WIN32_X64_H
+
+#include <brl.mod/blitz.mod/.bmx/blitz.bmx.release.win32.x64.h>
+#include <openb3dmax.mod/b3dglsdlgraphics.mod/.bmx/b3dglsdlgraphics.bmx.release.win32.x64.h>
+int _bb_main();
+BBINT _m_Terrain_MouseLook(struct openb3dmax_openb3dmax_TPivot_obj* bbt_pivot,struct openb3dmax_openb3dmax_TCamera_obj* bbt_camera,BBINT bbt_time,BBINT bbt_elapsed);
+
+#endif
---
+++ .bmx/Terrain.bmx.gui.release.win32.x64.i
@@ -0,0 +1,3 @@
+import brl.blitz
+import openb3dmax.b3dglsdlgraphics
+MouseLook%(pivot:TPivot,camera:TCamera,time%,elapsed%)="_m_Terrain_MouseLook"
